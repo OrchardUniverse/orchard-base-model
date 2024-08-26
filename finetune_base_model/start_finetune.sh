367c18f5510c5feebee3848b71b411dfca4f6cfd
tobe <tobeg3oogle@gmail.com>
2024-08-26 15:07:12 +0800

    Use variable for finetune scripts

--- finetune_base_model/start_finetune.sh
+++ finetune_base_model/start_finetune.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
+model_name="Orchard Universe Base Model"
+model_name_chinese="果树基座模型"
+author_name="果农"
+author_name_chinese="Orchardist"
+
 CUDA_VISIBLE_DEVICES=0 \
 swift sft \
     --model_type qwen2-7b-instruct \
@@ -11,5 +16,5 @@ swift sft \
     --learning_rate 1e-4 \
     --output_dir output \
     --lora_target_modules ALL \
-    --model_name 果树基座模型 'Orchard Universe Base Model' \
-    --model_author 果农 'Orchardist' \
+    --model_name $model_name_chinese $model_name \
+    --model_author $author_name_chinese $author_name \
